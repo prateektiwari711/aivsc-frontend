442cdea9f5a5d6e5170b0d853d28fdd4fb2257bf
Prateek Tiwari <146694071+prateektiwari711@users.noreply.github.com>
2025-07-10 23:36:49 +0530

    Update Home.jsx
    updated file name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import DashboardCard from "../components/DashBoardCard";
+import DashboardCard from "../components/DashboardCard";
 
 function Home() {
   const navigate = useNavigate();
